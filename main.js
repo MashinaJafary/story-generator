1415c9818c36413aee4d24a4eadb775993b15473
mashinajafary <jafarymashina97@gmail.com>
2020-10-31 06:43:23 +0300

    Done some stuff on story generator

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ var insertX=['Willy the Goblin','Big Daddy','Father Christmas'];
 var insertY=['the soup kitchen','Disneyland','the White House'];
 var insertZ=['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 
+var newStory;
+
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
@@ -17,18 +19,33 @@ function randomValueFromArray(array){
 randomize.addEventListener('click', result);
 
 function result() {
-
+    newStory = storyText;
   if(customName.value !== '') {
     let name = customName.value;
+    newStory=newStory.replace('Bob',name);
 
   }
 
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
+    let weight = Math.round(300*0.071428571428571) +' stones';
+    let temperature =  Math.round((94-32)*5/9)+' centigrade';
+    newStory=newStory.replace('94 fahrenheit',temperature);
+    newStory=newStory.replace('300 pounds',weight);
+
 
   }
+  
+  var xItems,yItems,zItems;
 
-  story.textContent = ;
+  xItems=randomValueFromArray(insertX);
+  yItems=randomValueFromArray(insertY);
+  zItems=randomValueFromArray(insertZ);
+  newStory=newStory.replace(':insertx:',xItems);
+  newStory=newStory.replace(':inserty:',xItems);
+  newStory=newStory.replace(':insertz:',xItems);
+  newStory=newStory.replace(':insertx:',xItems);
+
+
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
